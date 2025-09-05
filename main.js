32ea1d2e811043845a12e9dbc7cea761ce0336bb
Trnk7 <tarunkachhawa7@gmail.com>
2025-09-05 14:21:55 +0000

    deleted main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("message", (e) => {
-    if (e.data.type === "dblclicked") {
-        const mainCard = document.querySelector(".maincard");
-        if(mainCard==e.source.frameElement) return;
-        const tmpsrc = mainCard.src;
-        mainCard.src = e.source.frameElement.src ;
-        e.source.frameElement.src = tmpsrc;
-    }
-})
